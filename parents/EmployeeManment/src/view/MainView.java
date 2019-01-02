5a8aa8b0e2bc30e9355dae03bda0652b919e1263
meteor666666 <1362372344@qq.com>
2019-01-02 19:13:08 +0800

    first

---
+++ parents/EmployeeManment/src/view/MainView.java
@@ -0,0 +1,22 @@
+/**
+ * @Titil: MainView.java
+ * @Package:view
+ * @Description: TODO
+ * @Author: Andy
+ * @Create Date: 2019年1月2日 下午7:04:04
+ * @Version: 1.0 
+ */
+package view;
+
+/**
+ * @CLassName: MainView
+ * @Description: TODO
+ * @Author: Bluce
+ * @CreateDate: 2019年1月2日 下午7:04:04
+ *
+ */
+public class MainView {
+	public static void main(String[] args) {
+
+	}
+}
