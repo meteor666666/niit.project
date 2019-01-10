e640773fdfeba097c3dd9ab92499a25b4726f81b
meteor666666 <1362372344@qq.com>
2019-01-10 19:55:27 +0800

    ent

---
+++ parents/EmployeeManment/src/entity/Department.java
@@ -0,0 +1,31 @@
+package entity;
+
+public class Department {
+	private int id;
+	private String name;
+	private int empCount;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEmpCount() {
+		return empCount;
+	}
+
+	public void setEmpCount(int empCount) {
+		this.empCount = empCount;
+	}
+}
---
+++ parents/EmployeeManment/src/entity/Score.java
@@ -0,0 +1,50 @@
+package entity;
+
+public class Score {
+	private int id;
+	private Employee emp;
+	private Project pro;
+	private Integer value;
+	private String grade;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Employee getEmp() {
+		return emp;
+	}
+
+	public void setEmp(Employee emp) {
+		this.emp = emp;
+	}
+
+	public Project getPro() {
+		return pro;
+	}
+
+	public void setPro(Project pro) {
+		this.pro = pro;
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+}
