11a76b655a9ec06225e737d2d7b85de283249fcd
meteor666666 <1362372344@qq.com>
2019-01-11 20:50:14 +0800

    dao

---
+++ parents/EmployeeManment/src/dao/DepartmentDao.java
@@ -0,0 +1,205 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.Department;
+
+public class DepartmentDao extends BaseDao {
+
+	public List<Department> search() {
+		List<Department> list = new ArrayList<Department>();
+		Connection conn = null;
+		Statement stat = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			// 4 建立statement sql语句执行器
+			stat = conn.createStatement();
+			// 5 执行sql语句并得到结果
+			rs = stat.executeQuery("select * from department");
+			// 6 对结果集进行处理
+			while (rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("id"));
+				dep.setName(rs.getString("name"));
+				dep.setEmpCount(rs.getInt("emp_count"));
+				list.add(dep);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			// 7 关闭
+			closeAll(conn, stat, rs);
+		}
+
+		return list;
+	}
+
+	public List<Department> searchByCondition(Department condition) {
+		List<Department> list = new ArrayList<Department>();
+		Connection conn = null;
+		Statement stat = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			// 4 建立statement sql语句执行器
+			stat = conn.createStatement();
+			// 5 执行sql语句并得到结果
+			String where = " where 1=1 ";
+			if (!condition.getName().equals("")) {
+				where += " and name='" + condition.getName() + "'";
+			}
+			if (condition.getEmpCount() != -1) {
+				where += " and emp_count=" + condition.getEmpCount();
+			}
+			String sql = "select * from department " + where;
+			rs = stat.executeQuery(sql);
+			// 6 对结果集进行处理
+			while (rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("id"));
+				dep.setName(rs.getString("name"));
+				dep.setEmpCount(rs.getInt("emp_count"));
+				list.add(dep);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 7 关闭
+			closeAll(conn, stat, rs);
+		}
+		return list;
+	}
+
+	public boolean add(Department dep) {
+		boolean flag = false;
+		Connection conn = null;
+		Statement stat = null;
+		try {
+			conn = getConnection();
+			// 4 建立statement sql语句执行器
+			stat = conn.createStatement();
+			// 5 执行sql语句并得到结果
+			int rs = stat.executeUpdate("insert into department(name) values('" + dep.getName() + "')");
+			// 6 对结果集进行处理
+			if (rs > 0) {
+				flag = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeAll(conn, stat, null);
+		}
+		return flag;
+	}
+
+	public boolean update(Department dep) {
+		boolean flag = false;
+		Connection conn = null;
+		PreparedStatement pstat = null;
+		try {
+			conn = getConnection();
+			// 4 建立statement sql语句执行器
+			// Statement stat = conn.createStatement();
+			// 5 执行sql语句并得到结果
+			// String sql = "update department set name='" + selectEmp.getName() + "',sex='"
+			// +
+			// selectEmp.getSex() + "',age="
+			// + selectEmp.getAge() + " where id=" + selectEmp.getId();
+			// System.out.println(sql);
+			// int rs = stat.executeUpdate(sql);
+			String sql = "update department set name=? where id=?";
+			pstat = conn.prepareStatement(sql);
+			pstat.setString(1, dep.getName());
+			pstat.setInt(2, dep.getId());
+			int rs = pstat.executeUpdate();
+			// 6 对结果集进行处理
+			if (rs > 0) {
+				flag = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeAll(conn, pstat, null);
+		}
+		return flag;
+	}
+
+	public boolean delete(int id) {
+		boolean flag = false;
+		Connection conn = null;
+		PreparedStatement pstat = null;
+		try {
+			conn = getConnection();
+			conn.setAutoCommit(false);
+			String sql = "delete from department where id=?";
+			pstat = conn.prepareStatement(sql);
+			pstat.setInt(1, id);
+			int rs = pstat.executeUpdate();
+			pstat.close();
+			sql = "update employee set d_id=null where d_id=?";
+			pstat = conn.prepareStatement(sql);
+			pstat.setInt(1, id);
+			rs = pstat.executeUpdate();
+
+			pstat.close();
+			sql = "delete from r_dep_pro where d_id=?";
+			pstat = conn.prepareStatement(sql);
+			pstat.setInt(1, id);
+			rs = pstat.executeUpdate();
+			conn.commit();
+			// conn.setAutoCommit(true);
+			// 6 对结果集进行处理
+			if (rs > 0) {
+				flag = true;
+			}
+
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		} finally {
+			closeAll(conn, pstat, null);
+		}
+		return flag;
+	}
+
+	public boolean delete(String ids) {
+		boolean flag = false;
+		Connection conn = null;
+		Statement stat = null;
+		try {
+			conn = getConnection();
+			String sql = "delete from department where id in (" + ids + ")";
+			stat = conn.createStatement();
+
+			int rs = stat.executeUpdate(sql);
+			// 6 对结果集进行处理
+			if (rs > 0) {
+				flag = true;
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeAll(conn, stat, null);
+		}
+		return flag;
+	}
+
+}
