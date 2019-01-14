97bcca503b7396c41637ad8ada7b4d748e44a4b6
meteor666666 <1362372344@qq.com>
2019-01-14 11:32:13 +0800

    main，gai

--- parents/EmployeeManment/src/view/MainView.java
+++ parents/EmployeeManment/src/view/MainView.java
@@ -1,22 +1,82 @@
-/**
- * @Titil: MainView.java
- * @Package:view
- * @Description: TODO
- * @Author: Andy
- * @Create Date: 2019Âπ¥1Êúà2Êó• ‰∏ãÂçà7:04:04
- * @Version: 1.0 
- */
 package view;
 
-/**
- * @CLassName: MainView
- * @Description: TODO
- * @Author: Bluce
- * @CreateDate: 2019Âπ¥1Êúà2Êó• ‰∏ãÂçà7:04:04
- *
- */
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import view.department.DepartmentView;
+import view.employee.EmployeeView;
+import view.project.ProjectView;
+import view.score.ScoreView;
+
 public class MainView {
+
+	public void init() {
+		JFrame frame = new JFrame();
+		frame.setSize(400, 260);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		JPanel panel = (JPanel) frame.getContentPane();
+		panel.setLayout(new FlowLayout());
+		JButton empBtn = new JButton();
+		empBtn.setText("‘±π§π‹¿Ì");
+		empBtn.setPreferredSize(new Dimension(180, 90));
+		panel.add(empBtn);
+		empBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new EmployeeView().init();
+
+			}
+		});
+		JButton depBtn = new JButton();
+		depBtn.setText("≤ø√≈π‹¿Ì");
+		depBtn.setPreferredSize(new Dimension(180, 90));
+		panel.add(depBtn);
+		depBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new DepartmentView().init();
+
+			}
+		});
+		JButton proBtn = new JButton();
+		proBtn.setText("œÓƒøπ‹¿Ì");
+		proBtn.setPreferredSize(new Dimension(180, 90));
+		panel.add(proBtn);
+		proBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new ProjectView().init();
+
+			}
+		});
+		JButton scBtn = new JButton();
+		scBtn.setText("º®–ßπ‹¿Ì");
+		scBtn.setPreferredSize(new Dimension(180, 90));
+		panel.add(scBtn);
+		scBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ScoreView sv = new ScoreView();
+				sv.init();
+			}
+		});
+		frame.setVisible(true);
+
+	}
+
 	public static void main(String[] args) {
 
+		new MainView().init();
 	}
 }
